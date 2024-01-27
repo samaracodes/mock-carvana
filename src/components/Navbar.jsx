03c2370529a794376c5ac8d8f6dbd0300e641f0c
Sam <samaracodes@gmail.com>
2024-01-27 13:40:28 -0500

    implement navbar brand, links, styling

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,40 @@
+import {React, ReactDOM} from 'react'
+import { NavLink } from 'react-router-dom'
+import './navbar.css'
+
+const Navbar = () => {
+    return (
+        <>
+            <nav class="navbar fixed-top navbar-expand-md">
+                <div class="container-fluid">
+                    <a class="navbar-brand" href="#">
+                        <img src="/src/assets/CarvanaLOGO1.png" alt="Carvana Brand Logo" width="155"/>
+                    </a>
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                        <NavLink class="nav-link" href="#" style={{ color: "#183558", fontWeight: "700" }}>SEARCH CARS</NavLink>
+                        </li>
+                        <li class="nav-item">
+                        <NavLink class="nav-link" href="#" style={{ color: "#183558", fontWeight: "700" }}>SELL/TRADE</NavLink>
+                        </li>
+                        <li class="nav-item">
+                        <NavLink class="nav-link" href="#" style={{ color: "#183558", fontWeight: "700" }} >FINANCING</NavLink>
+                        </li>
+                    </ul>
+                    <ul class="navbar-nav ms-auto">
+                        <li class="nav-item">
+                            <NavLink class="nav-link" href="#" style={{ color: "#00AED9", fontWeight: "700" }}>SIGN IN</NavLink>
+                        </li>
+                    </ul>
+                    </div>
+                </div>
+                </nav>
+        </>
+    )
+}
+
+export default Navbar
